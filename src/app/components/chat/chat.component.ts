4585940c9a6bc6fca97c158da07b34f3ae8761d4
Diego Pagini <diego.pagini@neoris.com>
2022-05-05 22:03:06 +0200

    chat working

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ChatService } from 'src/app/services/chat.service';
 
 @Component({
@@ -7,21 +7,47 @@ import { ChatService } from 'src/app/services/chat.service';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss'],
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
   text: string = '';
-  messages$!: Observable<any>;
+  messages: any[] = [];
+  subscription!: Subscription;
+  element!: HTMLElement | any;
 
   constructor(private chatService: ChatService) {}
 
   ngOnInit(): void {
-    this.messages$ = this.chatService.getMessages();
+    /**
+     * Tomamos el elemento message de la vista.
+     */
+    this.element = document.getElementById('message');
+
+    this.subscription = this.chatService.getMessages().subscribe((message) => {
+      /**
+       * Cada vez que se detecta un nuevo mensaje este se agrega a la lista de mensajes y se hace scroll
+       * hasta el último recibido.
+       */
+      this.messages.push(message);
+      setTimeout(() => {
+        this.element.scrollTop = this.element.scrollHeight;
+      }, 50);
+    });
   }
 
   /**
    * Método para enviar el mensaje al servidor.
    */
   onSend(): void {
-    this.chatService.sendMessage(this.text);
-    this.text = '';
+    if (this.text.trim().length > 0) {
+      this.chatService.sendMessage(this.text);
+      this.text = '';
+    }
+  }
+
+  /**
+   * Eliminamos la subscripción cuando el componente se destruye.
+   */
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
